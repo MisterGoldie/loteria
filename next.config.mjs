5c36f1db6716ae00f51a60a42d73f4fc5e2e93ee
MisterGoldie <118089324+MisterGoldie@users.noreply.github.com>
2025-05-24 12:11:33 -0500

    had to go back again

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Handle www to non-www redirects
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.loteriagame.xyz',
-          },
-        ],
-        destination: 'https://loteriagame.xyz/:path*',
-        permanent: true,
-      },
-    ];
-  },
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
   webpack: (config) => {
